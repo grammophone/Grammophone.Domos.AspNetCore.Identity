184793fba640b44320e217bd67aa6d7e1c251db1
grammophone <emmanuel.liossis@gmail.com>
2024-04-26 15:25:01 -0400

    Add skeleton for BrowserSessionSignInManager.

---
+++ BrowserSessionSignInManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.Domos.DataAccess;
+using Grammophone.Domos.Domain;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Grammophone.Domos.AspNetCore.Identity
+{
+	/// <summary>
+	/// A sign-in manager that is able to handle browser sessions. The user manager must be a <see cref="BrowserSessionUserManager{UB, U, D}"/>.
+	/// </summary>
+	/// <typeparam name="UB">The base type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="U">The type of the user, derived from <typeparamref name="UB"/>.</typeparam>
+	/// <typeparam name="D">The type of the domain container, derived from <see cref="IUsersDomainContainer{U}"/>.</typeparam>
+	public class BrowserSessionSignInManager<UB, U, D> : SignInManager<U>
+		where UB : User
+		where U : UB
+		where D : IUsersDomainContainer<UB>
+	{
+		#region Construction
+
+		/// <inheritdoc/>
+		public BrowserSessionSignInManager(
+			BrowserSessionUserManager<UB, U, D> userManager,
+			IHttpContextAccessor contextAccessor,
+			IUserClaimsPrincipalFactory<U> claimsFactory,
+			IOptions<IdentityOptions> optionsAccessor,
+			ILogger<SignInManager<U>> logger,
+			IAuthenticationSchemeProvider schemes,
+			IUserConfirmation<U> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+		{
+			this.UserManager = userManager;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The user manager.
+		/// </summary>
+		public new BrowserSessionUserManager<UB, U, D> UserManager { get; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <inheritdoc/>
+		public override async Task SignOutAsync()
+		{
+			await base.SignOutAsync();
+		}
+
+		#endregion
+	}
+
+	/// <summary>
+	/// A sign-in manager that is able to handle browser sessions. The user manager must be a <see cref="BrowserSessionUserManager{U, U, D}"/>.
+	/// </summary>
+	/// <typeparam name="U">The type of the user, derived from <see cref="User"/>.</typeparam>
+	/// <typeparam name="D">The type of the domain container, derived from <see cref="IUsersDomainContainer{U}"/>.</typeparam>
+	public class BrowserSessionSignInManager<U, D> : BrowserSessionSignInManager<U, U, D>
+		where U : User
+		where D : IUsersDomainContainer<U>
+	{
+		/// <inheritdoc/>
+		public BrowserSessionSignInManager(
+			BrowserSessionUserManager<U, U, D> userManager,
+			IHttpContextAccessor contextAccessor,
+			IUserClaimsPrincipalFactory<U> claimsFactory,
+			IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<U>> logger,
+			IAuthenticationSchemeProvider schemes,
+			IUserConfirmation<U> confirmation)
+			: base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+		{
+		}
+	}
+}
